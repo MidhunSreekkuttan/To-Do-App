dab6ec5ccf33aad2b1de79b6634e1a9b6f43ae80
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-05-13 20:00:12 +0530

    Create a function for task creation,deletion and display that data

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,69 @@
+import TaskModel from "../data-base/models/taskModel.js"
+
+export const createTask = async (req, res) => {
+
+    try {
+
+        const userId = req.userId
+
+        const { title, description } = req.body
+        if (!title || !description) {
+            return res.json({ success: false, message: "enter credentials" })
+        }
+
+        const task = await TaskModel({
+            userId,
+            title,
+            description
+        })
+
+        await task.save()
+
+        res.json({ success: true, message: "Task created" })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+        console.log(error);
+    }
+
+}
+
+export const getTaskData = async (req, res) => {
+
+    try {
+
+        const userId = req.userId
+
+        const user = await TaskModel.findOne({ userId: userId })
+        if (!user) {
+            return res.json({ success: false, message: "user not found" })
+        }
+
+        res.json({ success: true, taskData: user })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+        console.log(error);
+    }
+
+}
+
+export const deleteTask = async (req, res) => {
+
+    try {
+
+        const userId = req.userId
+
+        const user = await TaskModel.findOneAndDelete({ userId: userId })
+        if (!user) {
+            return res.json({ success: false, message: "user not found" })
+        }
+
+        res.json({ success: true, message: "task deleted" })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+        console.log(error);
+    }
+
+}
