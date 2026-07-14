f2ff9091b5e4f457d233814540fada389a2d8df9
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-07-15 00:23:15 +0530

    take token and store to the localStorage from backend in login session

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,13 @@
-import { useState, useRef, useEffect } from "react"
+import { useState, useRef, useEffect, useContext } from "react"
 import { LabelField } from "../lib/bottons"
-import axios from 'axios'
 import axiosInstance from "../lib/AxiosInstance"
 import { toast } from 'react-hot-toast'
+import { Context } from "../lib/Context"
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+    const { setIsLoggedin } = useContext(Context)
 
     const [state, setState] = useState("login")
     const [formData, setFormData] = useState({
@@ -15,6 +18,8 @@ const Login = () => {
     const [loading, setLoading] = useState(false)
 
     const nameRef = useRef(null)
+
+    const navigate = useNavigate()
 
     const onChangeFn = (e) => {
         const { name, value } = e.target
@@ -32,6 +37,7 @@ const Login = () => {
         if (state === "signUp") {
 
             try {
+                setLoading(true)
 
                 const { data } = await axiosInstance.post('api/user/register', formData)
 
@@ -56,15 +62,21 @@ const Login = () => {
             } catch (error) {
                 console.log(error);
                 toast.error(error.message, { position: "top-right" })
+            } finally {
+                setLoading(false)
             }
 
         } else {
 
             try {
+                setLoading(true)
 
                 const { data } = await axiosInstance.post('api/user/login', formData)
 
                 if (data.success) {
+                    localStorage.setItem("LoginToken", data.token)
+                    setIsLoggedin(true)
+                    navigate(`/${data.userId}`)
                     toast.success("Loggin successfully", {
                         position: "top-right",
                         duration: 3000
@@ -83,98 +95,101 @@ const Login = () => {
             } catch (error) {
                 console.log(error);
                 toast.error(error.message, { position: "top-right" })
+            } finally {
+                setLoading(false)
             }
 
         }
 
-
     }
 
-    // useEffect(() => {
-    //     nameRef.current.focus()
-    // }, [])
-
     return (
-        <div className="w-screen h-screen flex justify-center items-center bg-rose-100">
+        <>
 
-            <div className="h-fit w-lg bg-amber-50 rounded-2xl p-5 px-15">
-                <h1 className="text-5xl font-bold text-center">
-                    {state === "login" ? "Login" : "SignUp"}
-                </h1>
+            {loading && <div>Loading...</div>}
 
-                <form onSubmit={submitHandler} className="flex flex-col gap-3 mt-5 pb-3">
-                    {state === "signUp" &&
+            <div className="w-screen h-screen flex justify-center items-center bg-rose-100">
+
+                <div className="h-fit w-lg bg-amber-50 rounded-2xl p-5 px-15">
+                    <h1 className="text-5xl font-bold text-center">
+                        {state === "login" ? "Login" : "SignUp"}
+                    </h1>
+
+                    <form onSubmit={submitHandler} className="flex flex-col gap-3 mt-5 pb-3">
+                        {state === "signUp" &&
+                            <LabelField
+                                ref={nameRef}
+                                text="Name"
+                                textColor="text-gray-600"
+                                name="name"
+                                value={formData.name}
+                                onChange={onChangeFn}
+                                placeholder="Enter your Name"
+                                type="text"
+                                textSize="text-lg"
+                                otherCss="focus:ring-rose-400 border border-gray-400"
+                            />
+                        }
+
                         <LabelField
-                            ref={nameRef}
-                            text="Name"
+                            text="Email"
                             textColor="text-gray-600"
-                            name="name"
-                            value={formData.name}
+                            name="email"
+                            value={formData.email}
                             onChange={onChangeFn}
-                            placeholder="Enter your Name"
-                            type="text"
+                            placeholder="Enter your Email"
+                            type="email"
                             textSize="text-lg"
                             otherCss="focus:ring-rose-400 border border-gray-400"
                         />
-                    }
 
-                    <LabelField
-                        text="Email"
-                        textColor="text-gray-600"
-                        name="email"
-                        value={formData.email}
-                        onChange={onChangeFn}
-                        placeholder="Enter your Email"
-                        type="email"
-                        textSize="text-lg"
-                        otherCss="focus:ring-rose-400 border border-gray-400"
-                    />
+                        <LabelField
+                            text="Password"
+                            textColor="text-gray-600"
+                            name="password"
+                            value={formData.password}
+                            onChange={onChangeFn}
+                            placeholder="Enter your Password"
+                            type="password"
+                            textSize="text-lg"
+                            otherCss="focus:ring-rose-400 border border-gray-400"
+                        />
 
-                    <LabelField
-                        text="Password"
-                        textColor="text-gray-600"
-                        name="password"
-                        value={formData.password}
-                        onChange={onChangeFn}
-                        placeholder="Enter your Password"
-                        type="password"
-                        textSize="text-lg"
-                        otherCss="focus:ring-rose-400 border border-gray-400"
-                    />
-
-                    {state === "login" && (
-                        <button type="button"
-                            className="text-sm text-slate-500 hover:text-slate-800 flex justify-end font-medium 
+                        {state === "login" && (
+                            <button type="button"
+                                className="text-sm text-slate-500 hover:text-slate-800 flex justify-end font-medium 
                                 transition-colors">
-                            Forgot Password?
+                                Forgot Password?
+                            </button>
+                        )}
+
+                        <button type="submit" className="w-full py-2 text-center bg-rose-200 rounded-lg text-2xl font-semibold mb-2">
+                            Submit
                         </button>
-                    )}
 
-                    <button type="submit" className="w-full py-2 text-center bg-rose-200 rounded-lg text-2xl font-semibold mb-2">
-                        Submit
-                    </button>
+                        <p className="flex justify-end">
+                            {state === "login" ? "If You have no accound:" : "You already have an accound:"}
+                            <span className="underline text-blue-600 cursor-pointer">
+                                {state === "login" ? (
+                                    <span onClick={() => {
+                                        setState("signUp")
+                                        setFormData({ name: "", email: "", password: "" })
+                                    }}>SignUp</span>
+                                ) : (
+                                    <span onClick={() => {
+                                        setState("login")
+                                        setFormData({ name: "", email: "", password: "" })
+                                    }}>Login</span>
+                                )}
+                            </span>
+                        </p>
 
-                    <p className="flex justify-end">
-                        {state === "login" ? "If You have no accound:" : "You already have an accound:"}
-                        <span className="underline text-blue-600 cursor-pointer">
-                            {state === "login" ? (
-                                <span onClick={() => {
-                                    setState("signUp")
-                                    setFormData({ name: "", email: "", password: "" })
-                                }}>SignUp</span>
-                            ) : (
-                                <span onClick={() => {
-                                    setState("login")
-                                    setFormData({ name: "", email: "", password: "" })
-                                }}>Login</span>
-                            )}
-                        </span>
-                    </p>
+                    </form>
+                </div>
 
-                </form>
             </div>
 
-        </div>
+        </>
     )
 }
 
