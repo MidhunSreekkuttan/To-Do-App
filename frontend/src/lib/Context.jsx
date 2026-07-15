e5e3a5be1d4bea9524b58c399972bdc281f7ab6f
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-07-15 09:41:21 +0530

    Create a fnction to get userData from backend to context API

--- frontend/src/lib/Context.jsx
+++ frontend/src/lib/Context.jsx
@@ -1,15 +1,58 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import axiosInstance from "./AxiosInstance";
 
 export const Context = createContext()
 
 const ContextProvider = ({ children }) => {
 
     const [isLoggedIn, setIsLoggedin] = useState(() => {
-        return !!localStorage.getItem('LoginToken')
+        return !!localStorage.getItem('loginToken')
     })
+    const [userData, setUserData] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+
+    console.log("userData", userData);
+
+    const getUserData = async () => {
+        const token = localStorage.getItem("loginToken")
+        if (!token) {
+            setUserData("")
+            setIsLoggedin(false)
+            return
+        }
+
+        try {
+
+            setIsLoading(true)
+
+            const { data } = await axiosInstance.get("/api/user/getUserData", {
+                headers: {
+                    "Authorization": token
+                }
+            })
+
+            if (data.success) {
+                setUserData(data?.user)
+            } else {
+                setUserData("")
+            }
+
+        } catch (error) {
+            console.log(error);
+            setUserData("")
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getUserData()
+    }, [])
 
     const values = {
-        isLoggedIn, setIsLoggedin
+        isLoggedIn, setIsLoggedin,
+        userData,
+        isLoading
     }
 
     return (
