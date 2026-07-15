653274359c2909f84506fe868537dcbe56109b60
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-07-15 23:53:00 +0530

    add resetPasswordToken and resetPasswordExpires fields in the usermodel

--- backend/data-base/models/userModel.js
+++ backend/data-base/models/userModel.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
+
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    resetPasswordToken: { type: String, default: "" },
+    resetPasswordExpires: { type: Date, default: 0 }
 }, { timestamps: true })
 
 const UserModel = mongoose.models.User || mongoose.model("User", userSchema)
