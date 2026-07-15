26fb5980f950fc87926c468119025898bf5b6d1a
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-07-15 23:55:39 +0530

    connect that function to the route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUserData, login, registration } from '../controllers/userController.js'
+import { forgotPassword, getUserData, login, registration, resetPassword } from '../controllers/userController.js'
 import userAuth from '../middleware/userAuth.js'
 
 const userRouter = express.Router()
@@ -7,5 +7,7 @@ const userRouter = express.Router()
 userRouter.post("/register", registration)
 userRouter.post("/login", login)
 userRouter.get("/getUserData", userAuth, getUserData)
+userRouter.post("/forgot-password", forgotPassword)
+userRouter.post("/reset-password/:token", resetPassword)
 
 export default userRouter
