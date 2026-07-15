fecc49eeeb5b41282cadc60776c46d8519381f88
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-07-15 13:56:00 +0530

    Setup the tanstack query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ContextProvider from './lib/Context.jsx'
 import './index.css'
 import App from './App.jsx'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ContextProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ContextProvider>
     </BrowserRouter>
   </StrictMode>,
