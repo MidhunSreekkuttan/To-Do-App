ef776a24fc6e76eacffc7a49f8a242c440c88454
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-07-17 00:16:39 +0530

    Add new user fields in User table

--- backend/data-base/models/userModel.js
+++ backend/data-base/models/userModel.js
@@ -2,12 +2,53 @@ import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
 
-    name: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    resetPasswordToken: { type: String, default: "" },
-    resetPasswordExpires: { type: Date, default: 0 },
-    profilePic: { type: String, default: "" }
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    profilePic: {
+        type: String,
+        default: ""
+    },
+
+    bio: {
+        type: String,
+        maxlength: 200,
+        default: ""
+    },
+
+    phone: {
+        type: String,
+        default: ""
+    },
+
+    location: {
+        type: String,
+        default: ""
+    },
+
+    resetPasswordToken: {
+        type: String,
+        default: ""
+    },
+
+    resetPasswordExpires: {
+        type: Date,
+        default: null
+    }
+
 }, { timestamps: true })
 
 const UserModel = mongoose.models.User || mongoose.model("User", userSchema)
