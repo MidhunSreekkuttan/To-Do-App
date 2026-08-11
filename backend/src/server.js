6d2fc2ace4f00758fb47f6b58d53f5093dd607b5
Midhun M <midhunsreekkuttan987@gmail.com>
2026-08-11 22:00:51 +0530

    convert frontend and backend in one URL instead of two

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,12 +6,15 @@ import dataBase from '../data-base/connectBD.js'
 import userRouter from '../routes/userRoute.js'
 import taskRouter from '../routes/taskRoute.js'
 import dns from "dns"
+import path from 'path'
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
+
+const __dirname = path.resolve()
 
 //midlewares
 app.use(express.json())
@@ -24,6 +27,18 @@ app.use("/api/task", taskRouter) //task routes
 
 await dataBase()
 
-app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.get(/.*/, (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+    })
+}
+
+if (process.env.NODE_ENV === "development") {
+    app.listen(PORT, () => {
+        console.log(`server is running on http://localhost:${PORT}`);
+    })
+}
+
+export default app
